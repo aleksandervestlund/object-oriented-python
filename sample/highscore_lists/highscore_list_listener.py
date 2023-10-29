929eff46d93b17439b0df9f57d99c8037c907b7a
Aleksander <aleksander.vestlund@hotmail.com>
2023-10-29 10:37:08 +0100

    Create tests folder

--- sample/highscore_lists/highscore_list_listener.py
+++
@@ -1,8 +0,0 @@
-from typing import Protocol
-
-from highscore_list import HighscoreList
-
-
-class HighscoreListListener(Protocol):
-    def list_changed(self, highscore_list: HighscoreList, idx: int) -> None:
-        ...
