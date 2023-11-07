540eb0858f15ed5976bfa346729272676a75e851
Aleksander <aleksander.vestlund@hotmail.com>
2023-11-07 15:19:16 +0100

    Complete TestPerson

--- tests/test_person.py
+++ tests/test_person.py
@@ -9,7 +9,7 @@ class TestPerson(TestCase):
         self.person_3 = Person("Jens", Gender.MALE)
         self.person_4 = Person("Marit", Gender.FEMALE)
 
-    def has_children(self, person: Person, children: list[Person]) -> None:
+    def _has_children(self, person: Person, children: list[Person]) -> None:
         self.assertEqual(len(children), person.get_child_count())
         for child in children:
             self.assertIn(child, person.children)
@@ -31,7 +31,7 @@ class TestPerson(TestCase):
 
         self.assertIsNone(self.person_1.father)
         self.assertIsNone(self.person_1.mother)
-        self.has_children(self.person_1, [self.person_3])
+        self._has_children(self.person_1, [self.person_3])
 
         self.assertEqual(self.person_3.father, self.person_1)
         self.assertIsNone(self.person_3.mother)
@@ -41,7 +41,7 @@ class TestPerson(TestCase):
 
         self.assertIsNone(self.person_1.father)
         self.assertIsNone(self.person_1.mother)
-        self.has_children(self.person_1, [self.person_3, self.person_4])
+        self._has_children(self.person_1, [self.person_3, self.person_4])
 
         self.assertEqual(self.person_3.father, self.person_1)
         self.assertIsNone(self.person_3.mother)
@@ -56,17 +56,17 @@ class TestPerson(TestCase):
 
         self.assertIsNone(self.person_1.father)
         self.assertIsNone(self.person_1.mother)
-        self.has_children(self.person_1, [self.person_3])
+        self._has_children(self.person_1, [self.person_3])
 
         self.assertEqual(self.person_3.father, self.person_1)
         self.assertIsNone(self.person_3.mother)
-        self.assertEqual(0, self.person_3.get_child_count())
+        self.assertEqual(self.person_3.get_child_count(), 0)
 
         self.person_1.add_child(self.person_4)
 
         self.assertIsNone(self.person_1.father)
         self.assertIsNone(self.person_1.mother)
-        self.has_children(self.person_1, [self.person_3, self.person_4])
+        self._has_children(self.person_1, [self.person_3, self.person_4])
 
         self.assertEqual(self.person_3.father, self.person_1)
         self.assertIsNone(self.person_3.mother)
@@ -81,24 +81,24 @@ class TestPerson(TestCase):
 
         self.assertIsNone(self.person_2.father)
         self.assertIsNone(self.person_2.mother)
-        self.has_children(self.person_2, [self.person_3])
+        self._has_children(self.person_2, [self.person_3])
 
         self.assertIsNone(self.person_3.father)
-        self.assertEqual(self.person_2, self.person_3.mother)
-        self.assertEqual(0, self.person_3.get_child_count())
+        self.assertEqual(self.person_3.mother, self.person_2)
+        self.assertEqual(self.person_3.get_child_count(), 0)
 
         self.person_4.mother = self.person_2
 
         self.assertIsNone(self.person_2.father)
         self.assertIsNone(self.person_2.mother)
-        self.has_children(self.person_2, [self.person_3, self.person_4])
+        self._has_children(self.person_2, [self.person_3, self.person_4])
 
         self.assertIsNone(self.person_3.father)
         self.assertEqual(self.person_3.mother, self.person_2)
         self.assertEqual(self.person_3.get_child_count(), 0)
 
         self.assertIsNone(self.person_4.father)
-        self.assertEqual(self.person_2, self.person_4.mother)
+        self.assertEqual(self.person_4.mother, self.person_2)
         self.assertEqual(self.person_4.get_child_count(), 0)
 
     def test_mother_add_child(self) -> None:
@@ -106,68 +106,54 @@ class TestPerson(TestCase):
 
         self.assertIsNone(self.person_2.father)
         self.assertIsNone(self.person_2.mother)
-        self.has_children(self.person_2, [self.person_3])
+        self._has_children(self.person_2, [self.person_3])
 
         self.assertIsNone(self.person_3.father)
         self.assertEqual(self.person_3.mother, self.person_2)
-        self.assertEqual(0, self.person_3.get_child_count())
+        self.assertEqual(self.person_3.get_child_count(), 0)
 
         self.person_2.add_child(self.person_4)
 
         self.assertIsNone(self.person_2.father)
         self.assertIsNone(self.person_2.mother)
-        self.has_children(self.person_2, [self.person_3, self.person_4])
+        self._has_children(self.person_2, [self.person_3, self.person_4])
 
         self.assertIsNone(self.person_3.father)
         self.assertEqual(self.person_3.mother, self.person_2)
         self.assertEqual(self.person_3.get_child_count(), 0)
 
         self.assertIsNone(self.person_4.father)
-        self.assertEqual(self.person_2, self.person_4.mother)
+        self.assertEqual(self.person_4.mother, self.person_2)
         self.assertEqual(self.person_4.get_child_count(), 0)
 
     def test_change_father_set_father(self) -> None:
         self.person_4.father = self.person_3
-
-        self.assertEqual(self.person_3, self.person_4.father)
-        self.has_children(self.person_3, [self.person_4])
-
         self.person_4.father = self.person_1
 
         self.assertEqual(self.person_4.father, self.person_1)
         self.assertEqual(self.person_3.get_child_count(), 0)
-        self.has_children(self.person_1, [self.person_4])
+        self._has_children(self.person_1, [self.person_4])
 
     def test_change_father_add_child(self) -> None:
         self.person_3.add_child(self.person_4)
-
-        self.assertEqual(self.person_3, self.person_4.father)
-        self.has_children(self.person_3, [self.person_4])
-
         self.person_1.add_child(self.person_4)
 
         self.assertEqual(self.person_4.father, self.person_1)
         self.assertEqual(self.person_3.get_child_count(), 0)
-        self.has_children(self.person_1, [self.person_4])
+        self._has_children(self.person_1, [self.person_4])
 
     def test_change_mother_set_mother(self) -> None:
         self.person_3.mother = self.person_4
-
-        self.assertEqual(self.person_3.mother, self.person_4)
-        self.has_children(self.person_4, [self.person_3])
-
         self.person_3.mother = self.person_2
 
         self.assertEqual(self.person_3.mother, self.person_2)
         self.assertEqual(self.person_4.get_child_count(), 0)
-        self.has_children(self.person_2, [self.person_3])
+        self._has_children(self.person_2, [self.person_3])
 
     def test_change_mother_add_child(self) -> None:
         self.person_4.add_child(self.person_3)
-        self.assertEqual(self.person_4, self.person_3.mother)
-        self.has_children(self.person_4, [self.person_3])
-
         self.person_2.add_child(self.person_3)
-        self.assertEqual(self.person_2, self.person_3.mother)
+
+        self.assertEqual(self.person_3.mother, self.person_2)
         self.assertEqual(self.person_4.get_child_count(), 0)
-        self.has_children(self.person_2, [self.person_3])
+        self._has_children(self.person_2, [self.person_3])
